16ab8947320b0eb5024a5383b10075a96cd69def
barojas04 <barojas04@gmail.com>
2026-06-26 13:48:04 -0600

    Clase modelo unidad y migracion

---
+++ app/Models/Unidad.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Unidad extends Model {
+    protected $table = 'unidades';
+    protected $primaryKey = 'idUnidad';
+    public $incrementing = false;
+    protected $keyType = 'string';
+    protected $fillable = ['idUnidad', 'nombre'];
+}
